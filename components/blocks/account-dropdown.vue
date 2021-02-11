92824ff1c4d35387c4bfc0dd19fb041af0d4e8d9
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-02-11 13:50:22 +0100

    improve nav for logged users

--- components/blocks/account-dropdown.vue
+++ components/blocks/account-dropdown.vue
@@ -9,23 +9,18 @@
         @click="accountDropdownOpened = !accountDropdownOpened"
       >
         <span class="sr-only">Open user menu</span>
-        <!-- <img
-                  class="w-8 h-8 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=UbJG7I0AnK&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                /> -->
-        <svg
-          class="w-8 h-8"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
+        <img
+          v-if="this.$auth.$state.loggedIn"
+          class="w-8 h-8 rounded-full"
+          :src="this.$auth.$state.user.profile_photo_url"
+          :alt="this.$auth.$state.user.name"
+        />
+        <img
+          v-else
+          src="/images/user.svg"
+          class="w-8 h-8 rounded-full"
+          alt="avatar"
+        />
       </button>
     </div>
     <!--
@@ -46,40 +41,41 @@
         aria-orientation="vertical"
         aria-labelledby="user-menu"
       >
-        <nuxt-link
-          :to="{ name: 'sign-in' }"
-          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-          role="menuitem"
-          @click.native="closeAccountDropdown"
-        >
-          Sign in
-        </nuxt-link>
-        <nuxt-link
-          :to="{ name: 'sign-up' }"
-          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-          role="menuitem"
-          @click.native="closeAccountDropdown"
-        >
-          Sign up
-        </nuxt-link>
-        <!-- <a
-                  href="#"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  role="menuitem"
-                  >Your Profile</a
-                >
-                <a
-                  href="#"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  role="menuitem"
-                  >Settings</a
-                >
-                <a
-                  href="#"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  role="menuitem"
-                  >Sign out</a
-                > -->
+        <div v-if="this.$auth.$state.loggedIn">
+          <nuxt-link
+            :to="{ name: 'dashboard' }"
+            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            role="menuitem"
+            @click.native="closeAccountDropdown"
+          >
+            Dashboard
+          </nuxt-link>
+          <button
+            class="block w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100"
+            role="menuitem"
+            @click="logout"
+          >
+            Sign out
+          </button>
+        </div>
+        <div v-else>
+          <nuxt-link
+            :to="{ name: 'sign-in' }"
+            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            role="menuitem"
+            @click.native="closeAccountDropdown"
+          >
+            Sign in
+          </nuxt-link>
+          <nuxt-link
+            :to="{ name: 'sign-up' }"
+            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+            role="menuitem"
+            @click.native="closeAccountDropdown"
+          >
+            Sign up
+          </nuxt-link>
+        </div>
       </div>
     </transition>
   </div>
@@ -98,6 +94,14 @@ export default {
     }
   },
   methods: {
+    async logout() {
+      try {
+        this.accountDropdownOpened = false
+        await this.$auth.logout()
+      } catch (error) {
+        console.error(error)
+      }
+    },
     closeAccountDropdown() {
       this.accountDropdownOpened = false
     },
