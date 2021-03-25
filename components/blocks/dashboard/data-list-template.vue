44d78f29c6fcd9a608d2a42a26cd5700bd6633eb
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-03-25 11:30:13 +0100

    🎨 improve dashboard data

--- components/blocks/dashboard/data-list-template.vue
+++ components/blocks/dashboard/data-list-template.vue
@@ -1,59 +1,89 @@
 <template>
-  <div class="bg-white rounded-md shadow dark:bg-gray-800">
+  <div>
     <div
-      class="px-4 py-5 border-b border-gray-200 dark:border-gray-700 sm:px-6 rounded-t-md"
+      class="px-4 py-5 border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700 sm:px-6 rounded-t-md"
     >
       <h3 class="flex items-center text-lg font-medium leading-6 text-gray-900">
-        <!-- <icon-heart class="text-red-600" :is-full="true" />
-        <div class="ml-2">Favorites list</div> -->
-        <slot name="title" />
+        <slot name="icon" />
+        <div class="ml-2">
+          {{ title }}
+        </div>
       </h3>
-      <div class="mt-1 text-sm text-gray-500">
-        <slot name="subtitle" />
+      <div class="mt-1 text-sm text-gray-500 dark:text-gray-400">
+        {{ subtitle }}
       </div>
     </div>
-    <div class="p-3 lg:p-5">
-      <transition name="fade">
-        <div v-if="list.length > 0">
-          <div v-for="data in list" :key="data.id" class="grid grid-cols-6">
-            <nuxt-link
-              :to="{
-                name: `${data.meta.for}s-slug`,
-                params: {
-                  author: data.meta.author,
-                  slug: data.meta.slug,
-                },
-              }"
-              class="relative flex items-center justify-between w-full col-span-5 py-0 my-1 text-gray-900 transition-colors duration-100 rounded-md dark:text-white dark:hover:bg-gray-700 hover:bg-gray-100"
-            >
-              <div class="relative flex items-center w-full">
-                <div v-if="withImage" class="h-20">
-                  <img
-                    v-lazy="data.image"
-                    alt="Book cover"
-                    class="object-cover object-center w-12 h-full rounded-sm shadow"
-                  />
-                </div>
-                <div class="flex flex-wrap items-center w-full py-1 ml-3">
-                  <slot name="content" :data="data" />
+    <ul v-if="list.length" class="divide-y divide-gray-200">
+      <nuxt-link
+        v-for="data in list"
+        :key="data.id"
+        :to="{
+          name: `${data.meta.for}s-slug`,
+          params: {
+            author: data.meta.author,
+            slug: data.meta.slug,
+          },
+        }"
+        class="relative flex items-center justify-between px-4 py-5 space-x-6 bg-white dark:bg-gray-800 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600"
+      >
+        <div class="flex items-center space-x-6">
+          <div class="flex-shrink-0">
+            <img
+              v-lazy="data.picture"
+              class="object-cover w-16 h-16 rounded-full"
+              :alt="data.title"
+            />
+          </div>
+          <div>
+            <div class="flex justify-between space-x-3">
+              <div class="flex-1 min-w-0">
+                <div class="block focus:outline-none">
+                  <!-- <span class="absolute inset-0" aria-hidden="true"></span> -->
+                  <p
+                    class="text-base font-semibold text-gray-900 truncate dark:text-gray-100"
+                  >
+                    {{ data.title }}
+                  </p>
+                  <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                    {{ $capitalize(data.meta.for) }}
+                  </p>
                 </div>
               </div>
-            </nuxt-link>
-            <div class="flex justify-center">
-              <button
-                class="flex w-full h-full text-gray-400 md:p-5 hover:text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                title="Delete"
-                @click="deleted(data)"
-              >
-                <icon-trash class="w-6 h-6 m-auto" />
-              </button>
+              <div class="hidden lg:block">
+                <time
+                  v-if="data.createdAt && !data.updatedAt"
+                  :datetime="data.createdAt"
+                  class="flex-shrink-0 text-sm text-gray-500 dark:text-gray-400 whitespace-nowrap"
+                  >{{ $getDate(data.createdAt) }}</time
+                >
+                <time
+                  v-if="data.updatedAt"
+                  :datetime="data.updatedAt"
+                  class="flex-shrink-0 text-sm text-gray-500 dark:text-gray-400 whitespace-nowrap"
+                  >{{ $getDate(data.updatedAt) }}</time
+                >
+              </div>
+            </div>
+            <div v-if="data.text" class="hidden mt-1 lg:block">
+              <p class="text-sm text-gray-600 dark:text-gray-300 line-clamp-2">
+                {{ data.text }}
+              </p>
             </div>
           </div>
         </div>
-        <div v-else class="flex flex-wrap items-center py-4 text-gray-400">
-          <slot name="no-data" />
+        <div class="flex justify-center">
+          <button
+            class="flex w-full h-full text-gray-400 md:p-5 hover:text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+            title="Delete"
+            @click="deleted(data)"
+          >
+            <icon-trash class="w-6 h-6 m-auto" />
+          </button>
         </div>
-      </transition>
+      </nuxt-link>
+    </ul>
+    <div v-else class="flex flex-wrap items-center py-4 text-gray-400">
+      {{ emptyList }}
     </div>
   </div>
 </template>
@@ -68,9 +98,17 @@ export default {
       type: Array,
       default: () => [],
     },
-    withImage: {
-      type: Boolean,
-      default: true,
+    title: {
+      type: String,
+      default: '',
+    },
+    subtitle: {
+      type: String,
+      default: '',
+    },
+    emptyList: {
+      type: String,
+      default: '',
     },
   },
   data() {
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -2,91 +2,38 @@
   <div class="container max-w-7xl">
     <section-heading
       title="Dashboard"
-      :subtitle="`Welcome ${this.$auth.user.name}`"
+      :subtitle="`Welcome ${$auth.user.name}`"
       :image="$auth.$state.user.profile_photo_url"
     />
     <div
       class="grid max-w-3xl grid-cols-1 gap-6 mx-auto mt-8 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2"
     >
       <div class="space-y-6 lg:col-start-1 lg:col-span-1">
-        <data-list-template :data-list="favorites" @deleted="deleted">
-          <template #title>
+        <data-list-template
+          :data-list="favorites"
+          title="Favorites list"
+          subtitle="All your favorites books will be here."
+          empty-list="Your favorite list is empty, try to favorite any book."
+          @deleted="deleted"
+        >
+          <template #icon>
             <icon-heart class="text-red-600" :is-full="true" />
-            <div class="ml-2">Favorites list</div>
-          </template>
-          <template #subtitle>
-            All your favorites books will be here.
-          </template>
-          <template #no-data>
-            Your favorite list is empty, try to favorite any book with
-            <icon-heart :is-full="true" />
-          </template>
-          <template #content="{ data }">
-            <div
-              class="block w-full font-semibold transition-colors duration-100"
-            >
-              <span class="hidden md:block">
-                {{ data.title }}
-              </span>
-              <span class="hidden sm:block md:hidden">
-                {{ $overflow(data.title, 20) }}
-              </span>
-              <span class="block sm:hidden">
-                {{ $overflow(data.title, 16) }}
-              </span>
-            </div>
           </template>
         </data-list-template>
       </div>
       <data-list-template
         :data-list="comments"
         :with-image="false"
+        title="Comments list"
+        subtitle="All your comments will be here."
+        empty-list="Your comments list is empty, try to comment any book."
         @deleted="deleted"
       >
-        <template #title>
-          <icon-heart class="text-red-600" :is-full="true" />
-          <div class="ml-2">Comments list</div>
-        </template>
-        <template #subtitle> All your comments will be here. </template>
-        <template #no-data>
-          Your comments list is empty, try to comment any book.
-        </template>
-        <template #content="{ data }">
-          <div>
-            <div class="flex items-center">
-              <img
-                v-lazy="data.image"
-                alt="Book cover"
-                class="object-cover object-center w-16 h-16 rounded-sm shadow"
-              />
-              <div class="ml-3 font-semibold">
-                {{ data.title }}
-              </div>
-            </div>
-            <div
-              class="flex w-full overflow-hidden italic text-gray-700 transition-colors duration-100 dark:text-gray-300"
-            >
-              <span
-                class="w-full"
-                v-html="`${$overflow(data.text, 250)}`"
-              ></span>
-            </div>
-
-            <div class="items-center hidden mt-2 text-sm text-gray-400 md:flex">
-              <div>
-                {{ $getDate(data.createdAt) }}
-              </div>
-              <div v-if="data.createdAt !== data.updatedAt">
-                {{ $getDate(data.updatedAt) }}
-              </div>
-            </div>
-          </div>
+        <template #icon>
+          <icon-comment class="w-6 h-6 text-blue-600" />
         </template>
       </data-list-template>
     </div>
-    <div
-      class="grid grid-cols-1 space-y-4 lg:space-y-0 lg:space-x-4 lg:grid-cols-2"
-    ></div>
   </div>
 </template>
 
@@ -97,12 +44,14 @@ import IconHeart from '~/components/icons/icon-heart.vue'
 
 import favorites from '~/mixins/favorites'
 import comments from '~/mixins/comments'
+import IconComment from '~/components/icons/icon-comment.vue'
 export default {
   name: 'Dashboard',
   components: {
     sectionHeading,
     IconHeart,
     DataListTemplate,
+    IconComment,
   },
   mixins: [favorites, comments],
   middleware: 'auth',
