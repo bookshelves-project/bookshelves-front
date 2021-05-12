7994ae6c2442d716db144b889195db029fc74772
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-05-12 20:52:48 +0200

    fix guides

--- pages/guides/index.vue
+++ pages/guides/index.vue
@@ -24,32 +24,9 @@
               ></component>
             </div>
             <div class="flex-1 min-w-0 px-4 md:grid md:grid-cols-2 md:gap-4">
-              <div>
-                <div
-                  class="flex items-center mt-2 text-sm text-gray-500 dark:text-gray-300"
-                >
-                  <div class="text-gray-500 text-opacity-25">
-                    <svg
-                      class="w-6 h-6"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                  </div>
-                  <span class="ml-1 truncate">
-                    {{ guide.category }}
-                  </span>
-                </div>
-                <h2 class="text-xl font-medium truncate text-primary-600">
-                  {{ guide.title }}
-                </h2>
-              </div>
+              <h2 class="my-auto text-xl font-medium text-primary-600">
+                {{ guide.title }}
+              </h2>
               <div class="hidden md:block">
                 <div>
                   <p class="text-sm text-gray-500 dark:text-gray-100">
