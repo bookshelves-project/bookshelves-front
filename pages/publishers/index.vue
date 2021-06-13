b086b101842df9e03c7ea4a722141b349defd04a
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-06-13 12:38:57 +0200

    add publishers index

--- pages/publishers/index.vue
+++ pages/publishers/index.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>PagePublishers</div>
+  <div class="container max-w-7xl">
+    <section-heading :title="title" :subtitle="description" />
+    <div>
+      <section class="flex flex-wrap items-center">
+        <nuxt-link
+          v-for="publisher in publishers"
+          :key="publisher.id"
+          :to="{ name: 'publishers-slug', params: { slug: publisher.slug } }"
+          class="px-3 py-1 m-1 transition-colors duration-100 bg-gray-300 rounded-full hover:bg-primary-300"
+        >
+          {{ publisher.name }} ({{ publisher.count }})
+        </nuxt-link>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
+import sectionHeading from '~/components/blocks/section-heading.vue'
 export default {
   name: 'PagePublishers',
+  components: { sectionHeading },
+  async asyncData({ app }) {
+    const publishers = await app.$axios.$get(`/publishers`)
+
+    return {
+      publishers: publishers.data,
+    }
+  },
+  data() {
+    return {
+      title: 'Publishers',
+      description: '',
+    }
+  },
 }
 </script>
