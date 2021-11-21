5fc1c3ac54650b544932e882ca1befa2132f6077
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-11-21 21:58:06 +0100

    💄 improve layouts and home page

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,35 +3,24 @@
     <lazy-layout-overlay />
     <lazy-special-routes-list />
     <lazy-app-notifications />
-    <div class="mx-auto bg-white dark:bg-gray-900">
+    <div class="app mx-auto">
       <lazy-layout-layer />
       <layout-navbar />
       <lazy-layout-sidebar />
       <div style="padding-top: 4rem"></div>
-      <Nuxt class="min-height-content" />
+      <nuxt class="min-height-content" />
       <lazy-layout-footer />
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'LayoutDefault',
-  data() {
-    return {
-      dev: process.env.NODE_ENV !== 'production',
-    }
-  },
-}
+<script setup lang="ts">
+import { useContext } from '@nuxtjs/composition-api'
+
+const dev = useContext().isDev
 </script>
 
 <style lang="postcss">
-/* @variants group-hover, hover, focus, dark {
-  .shadow-navbar {
-    box-shadow: 0 4px 2px -2px rgba(211, 211, 211, 0.4);
-  }
-} */
-
 .min-height-content {
   min-height: 70vh;
 }
