eb159d75380fee3718afa0507ffddf2ef373fbaa
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-12-05 17:24:28 +0100

    🔥 clean app

--- components/app/img.vue
+++ components/app/img.vue
@@ -1,32 +1,26 @@
 <template>
-  <span ref="AppImg">
-    <span
-      :class="init ? 'opacity-100' : 'opacity-100'"
-      class="app-img transition-opacity duration-200 h-full block"
-    >
-      <transition name="fade">
-        <span
-          v-if="loading"
-          ref="Placeholder"
-          :class="placeholder"
-          class="absolute z-10 inset-0 transition-transform duration-100 block"
-          :style="color ? `background-color: ${color}` : ''"
-        ></span>
-      </transition>
-      <figure class="h-full">
-        <img
-          ref="Image"
-          v-lazy-load
-          :data-src="src"
-          :alt="meta"
-          :title="meta"
-          class="!m-0 !w-full h-full img"
-          @load="load"
-        />
-        <figcaption v-if="legend" class="dark:text-gray-50 mt-2 mx-2 mb-10">{{ legend }}</figcaption>
-      </figure>
-    </span>
-  </span>
+  <div class="relative">
+    <transition name="fade">
+      <span
+        v-if="loading"
+        ref="Placeholder"
+        :class="placeholder"
+        class="absolute z-10 inset-0 transition-transform duration-100 block"
+        :style="color ? `background-color: ${color}` : ''"
+      ></span>
+    </transition>
+    <figure class="h-full">
+      <img
+        v-lazy-load
+        :data-src="src"
+        :alt="meta"
+        :title="meta"
+        class="!m-0 !w-full h-full object-cover app-img"
+        @load="load"
+      />
+      <figcaption v-if="legend" class="dark:text-gray-50 mt-2 mx-2 mb-10">{{ legend }}</figcaption>
+    </figure>
+  </div>
 </template>
 
 <script>
@@ -67,14 +61,14 @@ export default {
       meta: null
     }
   },
-  computed: {
-    titleData() {
-      if (!this.title) {
-        return this.altTitle
-      }
-      return this.title
-    }
-  },
+  // computed: {
+  //   titleData() {
+  //     if (!this.title) {
+  //       return this.altTitle
+  //     }
+  //     return this.title
+  //   }
+  // },
   watch: {
     src(newValue, oldValue) {
       //   this.setStyle()
@@ -82,38 +76,38 @@ export default {
     }
   },
   mounted() {
-    this.setup()
+    // this.setup()
     // TODO
     // auto alt and title
   },
   methods: {
-    setup() {
-      this.setMeta()
-      // this.setStyle()
-    },
-    setStyle() {
-      try {
-        /* eslint-disable no-unused-vars */
-        const appImg = this.$refs.AppImg
-        const placeholder = this.$refs.Placeholder
-        const image = this.$refs.Image
-        const style = appImg.classList
-        style.forEach((s) => {
-          // placeholder.classList.add(s)
-          image.classList.add(s)
-        })
-        //   this.init = false
-      } catch (error) { }
-    },
-    setMeta() {
-      try {
-        let title = this.src.split('/')
-        title = title[title.length - 1]
-        title = title.split('.')
-        title = title[0]
-        this.altTitle = title
-      } catch (error) { }
-    },
+    // setup() {
+    //   this.setMeta()
+    //   // this.setStyle()
+    // },
+    // setStyle() {
+    //   try {
+    //     /* eslint-disable no-unused-vars */
+    //     const appImg = this.$refs.AppImg
+    //     const placeholder = this.$refs.Placeholder
+    //     const image = this.$refs.Image
+    //     const style = appImg.classList
+    //     style.forEach((s) => {
+    //       // placeholder.classList.add(s)
+    //       image.classList.add(s)
+    //     })
+    //     //   this.init = false
+    //   } catch (error) { }
+    // },
+    // setMeta() {
+    //   try {
+    //     let title = this.src.split('/')
+    //     title = title[title.length - 1]
+    //     title = title.split('.')
+    //     title = title[0]
+    //     this.altTitle = title
+    //   } catch (error) { }
+    // },
     load() {
       this.loading = false
       this.meta = this.title
