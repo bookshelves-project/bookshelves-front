7472dee754b078dc3539d4ee7f572b632dd0db6f
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-04-09 09:57:07 +0200

    select field fix

--- components/field/select.vue
+++ components/field/select.vue
@@ -22,10 +22,12 @@ const selectOption = () => {
       class="block text-sm font-medium text-gray-700 dark:text-gray-200"
     >
       {{ label }}
+      <span v-if="required" class="text-red-600 dark:text-red-500">*</span>
     </label>
     <select
       :id="name"
       :name="name"
+      :required="required"
       v-model="selected"
       class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm rounded-md dark:bg-gray-700 dark:border-gray-700 dark:text-gray-200"
     >
