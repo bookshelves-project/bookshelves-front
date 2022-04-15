311acaf064b32189152a0c3969987b5b4dc0bc13
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-04-15 20:20:17 +0200

    review form wip

--- components/form/review.vue
+++ components/form/review.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import SvgIcon from '@/components/svg-icon.vue'
+import AppImg from '@/components/app/img.vue'
+import FieldText from '@/components/field/text.vue'
+import FieldRatingStars from '@/components/field/rating-stars.vue'
+
 const form = ref({
   text: '',
   rating: 0,
 })
+
+const config = useRuntimeConfig()
 
 const loggedIn = ref(false)
 const emit = defineEmits(['refresh'])
@@ -35,27 +42,27 @@ const submit = async () => {
   <div class="p-2">
     <transition>
       <div v-if="loggedIn" class="flex space-x-3">
-        <!-- <div class="shrink-0 hidden md:block">
-          <app-img
+        <!-- <field-review /> -->
+        <div class="shrink-0 hidden md:block">
+          <!-- <app-img
             :src="$auth.$state.user.data.avatar"
-            class="w-10 h-10"
-            override="rounded-full"
+            class="w-10 h-10 rounded-full"
             :alt="$auth.$state.user.data.username"
-          />
+          /> -->
         </div>
         <div class="flex-1 min-w-0">
           <form @submit.prevent="submit">
-            <field-text-input
+            <field-text
               id="text"
               v-model="form.text"
               name="text"
               rows="3"
-              maxlength="1500"
+              :maxlength="1500"
               placeholder="Add a review*"
               multiline
-            ></field-text-input>
+            />
             <field-rating-stars
-              v-if="$config.moduleSocialRating"
+              v-if="config.moduleSocialRating"
               v-model="form.rating"
               class="mt-1 mb-1"
             />
@@ -64,11 +71,11 @@ const submit = async () => {
                 href="https://www.markdownguide.org/cheat-sheet/"
                 target="_blank"
                 rel="noopener noreferrer"
-                class="inline-flex items-start space-x-2 text-sm text-gray-500 group hover:text-gray-900 dark:hover:text-gray-100"
+                class="flex items-center space-x-1 text-sm text-gray-500 hover:text-gray-900 dark:hover:text-gray-100"
               >
                 <svg-icon
                   name="question-mark-circle"
-                  class="shrink-0 w-5 h-5 text-gray-400 group-hover:text-gray-500"
+                  class="shrink-0 w-5 h-5"
                 />
                 <span>You can use Markdown.</span>
               </a>
@@ -82,11 +89,11 @@ const submit = async () => {
                 class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-600"
                 :disabled="form.text === ''"
               >
-                Review
+                Post a review
               </button>
             </div>
           </form>
-        </div> -->
+        </div>
       </div>
       <div v-else class="text-gray-400 italic">
         You must be logged in to post a review.
