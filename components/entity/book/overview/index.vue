3a5dbe4c76a86535e31ab446d9e46dd0b69fc351
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-04-15 20:18:50 +0200

    overview fix

---
+++ components/entity/book/overview/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import EntityBookRelated from '@/components/entity/book/related.vue'
+import EntityBookRelatedCss from '@/components/entity/book/related-css.vue'
+import EntityBookOverviewSmall from '@/components/entity/book/overview/small.vue'
+import EntityBookOverviewLarge from '@/components/entity/book/overview/large.vue'
+import EntityReview from '@/components/entity/review/index.vue'
+
+defineProps<{
+  book: Book
+}>()
+
+const related = ref(false)
+
+const success = (payload: boolean) => {
+  related.value = payload
+}
+</script>
+
+<template>
+  <div class="divide-y divide-gray-300 dark:divide-gray-700 space-y-10">
+    <div class="mx-auto lg:grid lg:grid-cols-3 lg:gap-x-8">
+      <entity-book-overview-small :book="book" />
+
+      <entity-book-overview-large :book="book" />
+    </div>
+    <div
+      v-if="
+        (book.tags && book.tags.length) || (book.genres && book.genres.length)
+      "
+      :class="related ? '' : 'hidden'"
+    >
+      <!-- <entity-book-related
+        v-if="book.tags?.length || book.genres?.length"
+        :book="book"
+        @success="success"
+      /> -->
+      <entity-book-related-css
+        v-if="book.tags?.length || book.genres?.length"
+        :book="book"
+        @success="success"
+      />
+    </div>
+    <div class="lg:col-span-2 lg:pr-8">
+      <entity-review :entity="book" />
+    </div>
+  </div>
+</template>
---
+++ components/entity/book/overview/large.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import EntityPublisherLink from '@/components/entity/publisher-link.vue'
+import EntityTagsLinks from '@/components/entity/tags-links.vue'
+import EntityBookOverviewFiles from '@/components/entity/book/overview/files.vue'
+import { date } from '~/utils/methods'
+
+defineProps<{
+  book: Book
+}>()
+</script>
+
+<template>
+  <div
+    class="col-span-2 order-1 lg:border-r lg:border-gray-200 dark:lg:border-gray-700 lg:pb-2 lg:pr-8"
+  >
+    <div class="flex flex-col h-full">
+      <div>
+        <div>
+          <h3 class="sr-only">Abstract</h3>
+
+          <div class="space-y-6">
+            <div
+              class="text-sm text-gray-900 dark:text-gray-100 word-wraping"
+              v-if="book.description"
+              v-html="book.description"
+            ></div>
+          </div>
+        </div>
+        <div :class="{ 'mt-10': book.description }">
+          <h3
+            class="text-sm font-medium text-gray-900 dark:text-gray-100 sr-only"
+          >
+            Details
+          </h3>
+
+          <div class="mt-4">
+            <ul role="list" class="data-list-details">
+              <li v-if="book.publisher">
+                <span>
+                  <span>Publisher:</span>
+                  <entity-publisher-link :publisher="book.publisher" />
+                  <span v-if="book.releasedOn"
+                    >({{ date(book.releasedOn) }})</span
+                  >
+                </span>
+              </li>
+              <li v-if="book.pageCount">
+                <span> Page count: {{ book.pageCount }} </span>
+              </li>
+              <li v-if="book.genres && book.genres.length >= 1">
+                <span>
+                  <span>Genre:</span>
+                  <entity-tags-links :tags="book.genres" short />
+                </span>
+              </li>
+              <li v-if="book.tags && book.tags.length >= 1">
+                <span>
+                  <span>Tags:</span>
+                  <entity-tags-links :tags="book.tags" short />
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <entity-book-overview-files :files="book.files" />
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.data-list-details {
+  @apply pl-4 list-disc text-sm space-y-2;
+  & li {
+    @apply text-gray-400 dark:text-gray-200;
+    & span {
+      @apply text-gray-600 dark:text-gray-400 flex items-center space-x-1;
+    }
+  }
+}
+</style>
