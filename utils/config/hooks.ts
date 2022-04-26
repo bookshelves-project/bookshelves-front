a86f700423ad5bee6511c55c708467809df81e14
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-04-26 20:20:14 +0200

    metadata improvement

--- utils/config/hooks.ts
+++ utils/config/hooks.ts
@@ -1,17 +1,4 @@
-import type { Plugin } from 'postcss'
-
-const order = (names: Plugin[]) => {
-  const tailwindNesting = names.splice(
-    names.findIndex((plugin) => plugin.postcssPlugin === 'tailwindcss/nesting'),
-    1
-  )[0]
-  names.unshift(tailwindNesting)
-}
-
 const hooks = {
-  // 'vite:extend'({ config }) {
-  //   order(config.css.postcss.plugins)
-  // },
   // 'content:file:beforeInsert': (document: any) => {
   //   if (document.extension === '.md') {
   //     // eslint-disable-next-line @typescript-eslint/no-var-requires
