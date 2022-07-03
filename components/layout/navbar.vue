71f93fcc27309406ed91271c0bcc01db5f5ed115
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-07-03 11:40:28 +0200

    fix

--- components/layout/navbar.vue
+++ components/layout/navbar.vue
@@ -13,56 +13,34 @@ const toggleSidebar = () => {
 </script>
 
 <template>
-  <nav
-    class="fixed z-30 w-full border-b border-primary-300 border-opacity-25 bg-white shadow-sm dark:bg-gray-800"
-  >
+  <nav class="fixed z-30 w-full border-b border-primary-300 border-opacity-25 bg-white shadow-sm dark:bg-gray-800">
     <div class="mx-auto max-w-7xl">
       <div class="relative flex h-16 items-center justify-between">
         <div class="h-full items-center px-2 flex lg:px-0">
           <div class="flex lg:hidden">
-            <button
-              aria-label="Open side menu"
-              @click="toggleSidebar"
-              class="hover:bg-gray-700 transition-colors duration-100 p-1.5 rounded-md"
-            >
-              <svg
-                class="block h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
+            <button aria-label="Open side menu" @click="toggleSidebar"
+              class="hover:bg-gray-700 transition-colors duration-100 p-1.5 rounded-md">
+              <svg class="block h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
                   d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
+                  clip-rule="evenodd"></path>
               </svg>
             </button>
           </div>
-          <router-link
-            :to="$localePath('/')"
-            class="ml-2 hidden lg:flex shrink-0 rounded-md p-2 transition-colors duration-75 hover:bg-gray-100 dark:hover:bg-gray-700"
-          >
+          <router-link :to="$localePath('/')"
+            class="ml-2 hidden lg:flex shrink-0 rounded-md p-2 transition-colors duration-75 hover:bg-gray-100 dark:hover:bg-gray-700">
             <!-- <img
               class="m-auto block h-8 w-8"
               src="/favicon.svg"
               :alt="config.appName"
             /> -->
-            <img
-              class="m-auto block h-8 w-8"
-              src="/icon.png"
-              :alt="config.appName"
-            />
+            <img class="m-auto block h-8 w-8" src="/icon.png" :alt="config.appName" />
           </router-link>
           <div class="hidden h-full lg:ml-4 lg:block">
             <div class="flex h-full space-x-6">
-              <router-link
-                :to="$localePath(link.route)"
-                v-for="(link, id) in navigation.main"
-                :key="id"
+              <router-link :to="$localePath(link.route)" v-for="(link, id) in navigation.main" :key="id"
                 class="focus-link inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-semibold text-gray-400 outline-none transition-colors duration-100 hover:border-gray-300 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white"
-                aria-current="page"
-              >
+                aria-current="page">
                 <span class="font-semibold">
                   {{ $t(`nav.${link.label}`) }}
                 </span>
--- types/search.d.ts
+++ types/search.d.ts
@@ -1,12 +1,12 @@
+declare interface EntityCollection {
+  authors?: Entity[]
+  series?: Entity[]
+  books?: Entity[]
+}
+
 declare interface Search {
   count?: number
   type?: 'collection' | 'meilisearch'
   relevant?: EntityCollection
   other?: EntityCollection
-}
-
-declare interface EntityCollection {
-  authors?: Entity[]
-  series?: Entity[]
-  books?: Entity[]
 }
