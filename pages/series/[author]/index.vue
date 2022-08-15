5ce26424674217517b48d872062e3a25ab7fa860
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-08-15 14:14:53 +0200

    redirect for series

--- pages/series/[author]/index.vue
+++ pages/series/[author]/index.vue
@@ -1,5 +1,33 @@
+<script setup lang="ts">
+const { params } = useRoute()
+
+definePageMeta({
+  middleware(to) {
+    if (to.params.author) {
+      const app = useNuxtApp()
+      const router = useRouter()
+      router.push(
+        app.$localePath({
+          name: 'authors-slug',
+          params: {
+            slug: to.params.author
+          }
+        })
+      )
+    }
+  }
+})
+</script>
+
 <template>
-  <div>
-    <pre>/series/[author]</pre>
-  </div>
+  <app-redirection
+    :to="
+      $localePath({
+        name: 'authors-slug',
+        params: {
+          slug: params.author,
+        },
+      })
+    "
+  />
 </template>
