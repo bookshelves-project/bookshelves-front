b224e22bd79bea9af2c209e4181a4395d51bd11d
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-08-23 16:10:01 +0200

    pagination fix

--- components/pagination/index.vue
+++ components/pagination/index.vue
@@ -42,13 +42,13 @@ watch(
       class="flex items-center justify-between border-t border-gray-200 px-4 dark:border-gray-700 sm:px-0"
     >
       <div class="-mt-px flex w-0 flex-1">
-        <app-link :to="linkGen(1)" class="item extreme-link">
+        <nuxt-link :to="linkGen(1)" class="item extreme-link">
           <svg-icon name="arrow-narrow-right" class="mr-1 w-5 h-5 rotate-90" />
           First
-        </app-link>
+        </nuxt-link>
       </div>
       <div class="hidden md:-mt-px md:flex">
-        <app-link v-slot="{ navigate }" :to="linkGen(current - 1)" custom>
+        <nuxt-link v-slot="{ navigate }" :to="linkGen(current - 1)" custom>
           <component
             :is="getTag(1)"
             :to="linkGen(current - 1)"
@@ -59,12 +59,12 @@ watch(
           >
             <svg-icon name="chevron-right" class="w-5 h-5 rotate-90" />
           </component>
-        </app-link>
+        </nuxt-link>
         <div v-if="showFirstDots" class="item dots">
           ...
         </div>
         <span v-for="pageNum in numberOfLinks" :key="pageNum + startNumber - 1">
-          <app-link
+          <nuxt-link
             :to="linkGen(pageNum + startNumber - 1)"
             :title="`Page ${pageNum + startNumber - 1}`"
             :aria-label="pageNum + startNumber - 1"
@@ -72,12 +72,12 @@ watch(
             :class="{ active: isActive(pageNum + startNumber - 1) }"
           >
             {{ pageNum + startNumber - 1 }}
-          </app-link>
+          </nuxt-link>
         </span>
         <div v-if="showLastDots" class="item dots">
           ...
         </div>
-        <app-link
+        <nuxt-link
           v-if="showLastDots && displayLastPage"
           v-slot="{ navigate }"
           :to="linkGen(1)"
@@ -93,8 +93,8 @@ watch(
           >
             {{ pages }}
           </component>
-        </app-link>
-        <app-link v-slot="{ navigate }" :to="linkGen(current + 1)" custom>
+        </nuxt-link>
+        <nuxt-link v-slot="{ navigate }" :to="linkGen(current + 1)" custom>
           <component
             :is="getTag(pages)"
             class="item page-link"
@@ -105,13 +105,13 @@ watch(
           >
             <svg-icon name="chevron-right" class="w-5 h-5" />
           </component>
-        </app-link>
+        </nuxt-link>
       </div>
       <div class="-mt-px flex w-0 flex-1 justify-end">
-        <app-link :to="linkGen(pages)" class="item extreme-link">
+        <nuxt-link :to="linkGen(pages)" class="item extreme-link">
           Last
           <svg-icon name="arrow-narrow-right" class="ml-1 w-5 h-5" />
-        </app-link>
+        </nuxt-link>
       </div>
     </nav>
   </div>
