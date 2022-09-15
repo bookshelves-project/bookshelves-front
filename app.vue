5532a43785042fb3a6f586b52458266b745d6593
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-09-15 17:47:27 +0200

    wip

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
+<script lang="ts" setup>
+// import { useApplicationStore } from '~/store/application'
+
+// const rc = useRuntimeConfig()
+const isDev = process.env.NODE_ENV === 'development'
+
+// const { nuxtInit } = useApplicationStore()
+// nuxtInit()
+</script>
+
 <template>
-  <div>
+  <div
+    :class="{ 'debug-screens': isDev }"
+    class="min-h-screen text-gray-900 transition-colors duration-200 dark:bg-gray-900 dark:text-gray-100"
+  >
+    <!-- <NuxtLoadingIndicator :color="rc.public.primaryColor500" />
+    <app-toasts />
+    <app-helper v-if="isDev" />
+    <app-preview />
+    <layout-navbar />
+    <layout-sidebar />
+    <div class="content-min-height pt-16">
+      <NuxtPage />
+    </div>
+    <layout-footer /> -->
     hey
     <svg-icon name="arrow-narrow-right" />
   </div>
