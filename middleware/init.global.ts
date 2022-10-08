e86ba57cdb94f835ba80ac39a2156ad44875c226
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-10-08 19:48:35 +0200

    wip

--- middleware/init.global.ts
+++ middleware/init.global.ts
@@ -1,3 +1,7 @@
+/**
+ * Nuxt: https://v3.nuxtjs.org/guide/directory-structure/middleware/
+ * Docs: https://github.com/nuxt/framework/discussions/1722
+ */
 export default defineNuxtRouteMiddleware((to, from) => {
   // if (to.params.id === '1') {
   //   return abortNavigation()
